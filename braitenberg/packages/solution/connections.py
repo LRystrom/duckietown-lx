80461f1f4ebbed10028435bb4f6a5dbb4dc9ada6
Larry Rystrom <larry.rystrom@gmail.com>
2023-10-21 12:00:38 -0700

    Experiments

--- braitenberg/packages/solution/connections.py
+++ braitenberg/packages/solution/connections.py
@@ -2,13 +2,19 @@ from typing import Tuple
 
 import numpy as np
 
+#left_motor  = const + gain *  np.sum( LEFT * preprocess(image) )
+#right_motor = const + gain *  np.sum( RIGHT * preprocess(image) )
+
 
 def get_motor_left_matrix(shape: Tuple[int, int]) -> np.ndarray:
     # TODO: write your function instead of this one
     res = np.zeros(shape=shape, dtype="float32")
-    # these are random values
-    res[100:150, 100:150] = 1
-    res[300:, 200:] = 1
+
+    res[:,0:320] = 1
+    #res.fill(1.0)
+    ## these are random values
+    #res[100:150, 100:150] = 1
+    #res[300:, 200:] = 1
     # ---
     return res
 
@@ -16,7 +22,10 @@ def get_motor_left_matrix(shape: Tuple[int, int]) -> np.ndarray:
 def get_motor_right_matrix(shape: Tuple[int, int]) -> np.ndarray:
     # TODO: write your function instead of this one
     res = np.zeros(shape=shape, dtype="float32")
+
+    res[:,320:640] = -1
+    #res.fill(1.0)
     # these are random values
-    res[100:150, 100:300] = -1
+    #res[100:150, 100:300] = -1
     # ---
     return res
